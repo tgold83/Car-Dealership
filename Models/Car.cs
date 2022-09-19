037378f02eb431abc301aa5961b7ec3e60c9b1f9
tgold83 <ted.jonathan.gold@gmail.com>
2022-09-19 09:31:25 -0700

    add code from lesson 10 into file
    Co-authored-by: Joseph Jackson <josephjacksonw@gmail.com>

--- Models/Car.cs
+++ Models/Car.cs
@@ -1,11 +1,37 @@
-namespace Dealership.Models
-{
+using System;
+
+namespace Dealership.Models {
+
   public class Car
   {
-    public string MakeModel;
-    public int Price;
-    public int Miles;
+    // Public auto-implemented properties
+    public string MakeModel { get; set; }
+    public int Price { get; set; }
+    public int Miles { get; set; }
 
+    // public string MakeModel
+    // {
+    //   get 
+    //   {
+    //     return "That's a big secret. You'll have to visit Lonny's to find out about our special deals!";
+    //   }
+    //   set 
+    //   {
+    //     MakeModel = value;
+    //   }
+    // }
+
+    public static string MakeSound(string sound)
+    {
+      return "our cars sound like " + sound;
+    }
+
+    public void SetPrice(int newPrice) 
+    {
+      Price = newPrice;
+    }
+
+    // Constructor
     public Car(string makeModel, int price, int miles)
     {
       MakeModel = makeModel;
@@ -13,6 +39,21 @@ namespace Dealership.Models
       Miles = miles;
     }
 
+    public string GetMakeModel()
+    {
+      return MakeModel;
+    }
+
+    public int GetPrice()
+    {
+      return Price;
+    }
+
+    public int GetMiles()
+    {
+      return Miles;
+    }
+    
     public bool WorthBuying(int maxPrice)
     {
       return (Price <= maxPrice);
--- Program.cs
+++ Program.cs
@@ -10,10 +10,18 @@ namespace Dealership
     {
       Car volkswagen = new Car("1974 Volkswagen Thing", 1100, 368792);
       Car yugo = new Car("1980 Yugo Koral", 700, 56000);
+      Console.WriteLine(yugo.MakeModel);
+      yugo.MakeModel = "Worst Car Ever";
+      Console.WriteLine(yugo.MakeModel);
+
+
       Car ford = new Car("1988 Ford Country Squire", 1400, 239001);
       Car amc = new Car("1976 AMC Pacer", 400, 198000);
 
       List<Car> Cars = new List<Car>() { volkswagen, yugo, ford, amc };
+
+      yugo.SetPrice(300);
+
 
       Console.WriteLine("Enter maximum price: ");
       string stringMaxPrice = Console.ReadLine();
@@ -31,7 +39,10 @@ namespace Dealership
 
       foreach(Car automobile in CarsMatchingSearch)
       {
-        Console.WriteLine(automobile.MakeModel);
+        Console.WriteLine("----------------------");
+        Console.WriteLine(automobile.GetMakeModel());
+        Console.WriteLine(automobile.GetMiles() + " miles");
+        Console.WriteLine("$" + automobile.GetPrice());
       }
     }
   }
